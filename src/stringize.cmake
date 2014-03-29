3264f0853cb4b940fd83e576de99f8aa159add0f
rconde <rconde01@outlook.com>
2014-03-29 19:23:08 -0400

    - fixed script to preserve newlines

--- src/stringize.cmake
+++ src/stringize.cmake
@@ -7,9 +7,7 @@ string(REPLACE "\"" "\\\"" SHADER_CONTENTS "${SHADER_CONTENTS}")
 # escape semicolons
 string(REGEX REPLACE ";" "\\\\;" SHADER_CONTENTS "${SHADER_CONTENTS}")
 # no blank lines allowed in cmake lists...so hack
-string(REGEX REPLACE "^\\n" "__BLANK_LINE_HACK__\n" SHADER_CONTENTS "${SHADER_CONTENTS}")
-# replace newlines with semicolons
-string(REGEX REPLACE "\n" ";" SHADER_CONTENTS "${SHADER_CONTENTS}")
+string(REGEX REPLACE "\n" "__BLANK_LINE_HACK__;" SHADER_CONTENTS "${SHADER_CONTENTS}")
 
 ### create variable name from the shader file name
 string(REPLACE "-" "_" SHADER_VAR ${SHADER_FILE})
@@ -18,11 +16,8 @@ string(REPLACE "." "_" SHADER_VAR ${SHADER_VAR})
 ### write out the header
 file(WRITE ${HEADER_FILE} "static const char * ${SHADER_VAR} = \n")
 foreach(_line ${SHADER_CONTENTS})
-	if("${_line}" STREQUAL "__BLANK_LINE_HACK__")
-		file(APPEND ${HEADER_FILE} "\"\\n\"\n")
-	else("${_line}" STREQUAL "__BLANK_LINE_HACK__")
-		file(APPEND ${HEADER_FILE} "\"${_line}\\n\"\n")
-	endif("${_line}" STREQUAL "__BLANK_LINE_HACK__")
+   string(REPLACE "__BLANK_LINE_HACK__" "" _line ${_line})
+   file(APPEND ${HEADER_FILE} "\"${_line}\\n\"\n")
 endforeach()
-file(APPEND ${HEADER_FILE} ";")
+file(APPEND ${HEADER_FILE} ";\n")
 
